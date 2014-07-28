38436b227191c99760511c3d0f249641f4ace065
jellysheep <max.mail@dameweb.de>
2014-07-28 22:44:39 +0200

    Updated Doxygen documentation.

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['server_5fpublish_5faddress',['SERVER_PUBLISH_ADDRESS',['../vsp__test_8c.html#a7d63f7b478aef2fa2e5700ee5531d7d9',1,'vsp_test.c']]],
   ['server_5fsubscribe_5faddress',['SERVER_SUBSCRIBE_ADDRESS',['../vsp__test_8c.html#a5153e30084813987e26f05abd280eb66',1,'vsp_test.c']]],
-  ['state',['state',['../structvsp__cmcp__client.html#a3eeff450faafe982d1d2c5036b0bf262',1,'vsp_cmcp_client::state()'],['../structvsp__cmcp__server.html#ae72070f7f3666185272c9058f50585ce',1,'vsp_cmcp_server::state()']]],
+  ['state',['state',['../structvsp__cmcp__client.html#a2873f762d878a71c0b14338d5cbf3ed1',1,'vsp_cmcp_client::state()'],['../structvsp__cmcp__server.html#a124a030b92243d47832f377c82e05a9d',1,'vsp_cmcp_server::state()']]],
   ['subscribe_5fsocket',['subscribe_socket',['../structvsp__cmcp__client.html#a0b0070acc001abd6b05fa689b672374e',1,'vsp_cmcp_client::subscribe_socket()'],['../structvsp__cmcp__server.html#a3cda1d0e091677be63dca9a576ed1ba2',1,'vsp_cmcp_server::subscribe_socket()']]]
 ];
