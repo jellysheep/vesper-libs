1bdd8b29d072f5ab3937531fd936dd0737dd17fc
jellysheep <max.mail@dameweb.de>
2014-08-10 13:44:08 +0200

    vesper_test: Added test for vsp_time functions.

--- vesper_test/vsp_test_util.c
+++ vesper_test/vsp_test_util.c
@@ -12,6 +12,7 @@
 
 #include <vesper_util/vsp_error.h>
 #include <vesper_util/vsp_random.h>
+#include <vesper_util/vsp_time.h>
 #include <vesper_util/vsp_util.h>
 #include <stdio.h>
 #include <string.h>
@@ -21,6 +22,9 @@ MU_TEST(vsp_test_error_test);
 
 /** Check output of pseudo-random number generator. */
 MU_TEST(vsp_test_random_test);
+
+/** Check time measurement functions. */
+MU_TEST(vsp_test_time_test);
 
 MU_TEST(vsp_test_error_test)
 {
@@ -59,8 +63,28 @@ MU_TEST(vsp_test_random_test)
     }
 }
 
+MU_TEST(vsp_test_time_test)
+{
+    double realtime_before, realtime_after;
+    double cputime_before, cputime_after;
+
+    realtime_before = vsp_time_real();
+    cputime_before = vsp_time_cpu();
+
+    do {
+        realtime_after = vsp_time_real();
+        cputime_after = vsp_time_cpu();
+    } while (realtime_after == realtime_before ||
+        cputime_after == cputime_before);
+
+    /* check if duration is positive */
+    mu_assert(realtime_after > realtime_before, "Real time values invalid.");
+    mu_assert(cputime_after > cputime_before, "CPU time values invalid.");
+}
+
 MU_TEST_SUITE(vsp_test_util)
 {
     MU_RUN_TEST(vsp_test_error_test);
     MU_RUN_TEST(vsp_test_random_test);
+    MU_RUN_TEST(vsp_test_time_test);
 }
